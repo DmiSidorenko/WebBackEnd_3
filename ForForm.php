d3a513dcbcf6bf493c909017b5c1c4eb1077a1e9
DmiSidorenko <90641562+DmiSidorenko@users.noreply.github.com>
2022-03-27 19:58:42 +0300

    Update ForForm.php

--- ForForm.php
+++ ForForm.php
@@ -48,22 +48,22 @@ $power5=in_array('more',$_POST['Superpowers']) ? '1' : '0';
 $power6=in_array('nothing',$_POST['Superpowers']) ? '1' : '0';
 $power='-';
 if ($power1=='1')
-    $power+='1 ';
+    $power.='1 ';
     else
     if ($power2=='1')
-        $power+='2 ';
+        $power.='2 ';
         else
         if ($power3=='1')
-            $power+='3 ';
+            $power.='3 ';
             else
             if ($power4=='1')
-                $power+='4 ';
+                $power.='4 ';
                 else
                 if ($power5=='1')
-                    $power+='5 ';
+                    $power.='5 ';
                     else
                     if ($power6=='1')
-                        $power+='6 ';
+                        $power.='6 ';
 
 $user = 'u41123';
 $pass = '1452343';
